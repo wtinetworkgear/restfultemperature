7feac3b07479b976ea710d19e7536e4b0cc91c00
ken Partridge <kenpartridge@kens-MacBook-Pro.local>
2018-05-05 18:54:09 -0700

    Updated and cleaned up the Python code

--- temp.py
+++ temp.py
@@ -1,35 +1,49 @@
-import urllib2, base64, json
+#!/usr/bin/env python
+
+import json
 import os, time
+import requests
+
+# supress Unverified HTTPS request, only do this is a verified environment
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # Address of the WTI device
-BASE_PATH = "https://192.168.0.158/api/v2/status/temperature"
+URI = "https://"
+SITE_NAME = "192.168.0.158"
+BASE_PATH = "/api/v2/status/temperature"
+
 # put in the username and password to yuor WTI device here
 USERNAME = "super"
 PASSWORD = "super"
 
 iCount = 0
-while (iCount < 100):
-	request = urllib2.Request(BASE_PATH)
+iFail  = 0
+while (iCount < 10000):
+	try:
+		r = requests.get(URI+SITE_NAME+BASE_PATH, auth=(USERNAME, PASSWORD), verify=False)
+		if (r.status_code == 200):
+			os.system('clear')
+			parsed_json = r.json()
 
-	base64string = base64.encodestring('%s:%s' % (USERNAME, PASSWORD)).replace('\n', '') 
-	request.add_header("Authorization", "Basic %s" % base64string)  
-	result = urllib2.urlopen(request)
+#			Uncomment to see the JSON return by the unit
+#			print parsed_json
 
-	cszTemp = result.read().strip()
-#	Uncomment to see the raw JSON return by the unit
-#	print cszTemp
+			cszTemperature = parsed_json['temperature']
+			cszTemperatureFormat = parsed_json['format']
+			cszTimeStamp = parsed_json['timestamp']
 
-	parsed_json = json.loads(cszTemp)
+			print("Current Temperature is")
+			print("----------------------")
+			print(cszTemperature+ " " +cszTemperatureFormat + " @ "+cszTimeStamp+"\r\n")
+			print(URI+SITE_NAME+BASE_PATH)
+		else:
+			print(r.status_code)
 
-	cszTemperature = parsed_json['temperature']
-	cszTemperatureFormat = parsed_json['format']
-	cszTimeStamp = parsed_json['timestamp']
-
-	os.system('clear')
-	print("Current Temperature is")
-	print("----------------------")
-	print(cszTemperature+ " " +cszTemperatureFormat + " @ "+cszTimeStamp)
-	
+	except requests.exceptions.RequestException as e:  # This is the correct syntax
+		print e
+		time.sleep(60)
+		iFail = iFail + 1
+	iCount = iCount + 1
+	print("iCount = "+str(iCount)+ ", iFail = "+str(iFail))
 	time.sleep(1)
-	iCount + iCount + 1
-
