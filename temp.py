6f8368dd3f9fd1026d83a0a9ae5a1fd0a4ffa9da
wtinetworkgear <kenp@wti.com>
2018-06-29 13:35:38 -0700

    Update temp.py

--- temp.py
+++ temp.py
@@ -17,33 +17,26 @@ BASE_PATH = "/api/v2/status/temperature"
 USERNAME = "rest"
 PASSWORD = "restfulpassword"
 
-iCount = 0
-iFail  = 0
-while (iCount < 10000):
-	try:
-		r = requests.get(URI+SITE_NAME+BASE_PATH, auth=(USERNAME, PASSWORD), verify=False)
-		if (r.status_code == 200):
-			os.system('clear')
-			parsed_json = r.json()
+try:
+	r = requests.get(URI+SITE_NAME+BASE_PATH, auth=(USERNAME, PASSWORD), verify=False)
+	if (r.status_code == 200):
+		os.system('clear')
+		parsed_json = r.json()
 
-#			Uncomment to see the JSON return by the unit
-#			print parsed_json
+#		Uncomment to see the JSON return by the unit
+#		print parsed_json
 
-			cszTemperature = parsed_json['temperature']
-			cszTemperatureFormat = parsed_json['format']
-			cszTimeStamp = parsed_json['timestamp']
+		cszTemperature = parsed_json['temperature']
+		cszTemperatureFormat = parsed_json['format']
+		cszTimeStamp = parsed_json['timestamp']
 
-			print("Current Temperature is")
-			print("----------------------")
-			print(cszTemperature+ " " +cszTemperatureFormat + " @ "+cszTimeStamp+"\r\n")
-			print(URI+SITE_NAME+BASE_PATH)
-		else:
-			print(r.status_code)
+		print("Current Temperature is")
+		print("----------------------")
+		print(cszTemperature+ " " +cszTemperatureFormat + " @ "+cszTimeStamp+"\r\n")
+		print(URI+SITE_NAME+BASE_PATH)
+	else:
+		print(r.status_code)
 
-	except requests.exceptions.RequestException as e:  # This is the correct syntax
-		print e
-		time.sleep(60)
-		iFail = iFail + 1
-	iCount = iCount + 1
-	print("iCount = "+str(iCount)+ ", iFail = "+str(iFail))
-	time.sleep(1)
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+	print (e)
+	time.sleep(60)
